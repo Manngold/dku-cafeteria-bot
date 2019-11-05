ae5244c9c73c5d7b18e81412b053f7692802207d
Manngold <jsw4820@gmail.com>
2019-11-05 20:29:00 +0900

    modify caffeine handler

--- src/feature/caffeine.js
+++ src/feature/caffeine.js
@@ -5,8 +5,9 @@ const caffeine = () => {
     app.get(routes.caffeine, caffeineHandler);
 };
 
-const caffeineHandler = () => {
+const caffeineHandler = (req, res) => {
     console.log("Wake Up! 🚀");
+    res.status(200).end();
 };
 
 export default caffeine;
